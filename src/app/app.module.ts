90c90250bef607f0995db508c187ae46a336a396
Haginus <hagiandrei.ah@gmail.com>
2022-02-10 15:27:49 +0200

    Add tip component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LabViewerComponent } from './pages/lab-viewer/lab-viewer.component';
 import { LabDirective } from './directives/lab.directive';
 import { LabListComponent } from './components/lab-list/lab-list.component';
 import { InfoComponent } from './components/info/info.component';
+import { TipComponent } from './components/tip/tip.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { InfoComponent } from './components/info/info.component';
     Lab1Component,
     LabListComponent,
     InfoComponent,
+    TipComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/tip/tip.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tip',
+  templateUrl: './tip.component.html',
+  styleUrls: ['./tip.component.scss']
+})
+export class TipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
