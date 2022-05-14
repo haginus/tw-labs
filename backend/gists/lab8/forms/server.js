0919aae43e8e4257036285ef0c268fb11fca4eb6
Haginus <hagiandrei.ah@gmail.com>
2022-05-15 02:00:17 +0300

    Fix forms gist

--- backend/gists/lab8/forms/server.js
+++ backend/gists/lab8/forms/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
-  res.sendFile('form.html');
+  res.sendFile(path.join(__dirname, 'form.html'));
 });
 
 app.post('/', function (req, res) {
